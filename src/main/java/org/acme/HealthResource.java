2af133a04b2da199352e71e9eebeec46a3bbbaa8
Yashwanth Maheshwaram <yashwanth34@gmail.com>
2022-10-14 17:33:18 +0530

    init commit

---
+++ src/main/java/org/acme/HealthResource.java
@@ -0,0 +1,17 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/health")
+public class HealthResource {
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String health(){
+        return "Health";
+    }
+    
+}
